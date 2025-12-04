0cde28157864e5ea72ffd05a0dcb9e6a9ccf1f66
chohj2614 <hyunjoon0614@naver.com>
2025-12-04 16:16:59 +0900

    spacing

--- DDL/WeLearning.sql
+++ DDL/WeLearning.sql
@@ -9,6 +9,7 @@ CREATE TABLE user (
     role EMUM('수강생','강사') NOT NULL DEFAULT '수강생'
     phone VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE instructors (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
@@ -17,8 +18,8 @@ CREATE TABLE instructors (
     rating DECIMAL NOT NULL,
     students BIGINT DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES user(id)
-     
 );
+
 CREATE TABLE course (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     ins_id BIGINT NOT NULL,
@@ -31,12 +32,14 @@ CREATE TABLE course (
     rating DECIMAL NOT NULL,
     students BIGINT DEFAULT 0
 );
+
 CREATE TABLE cos_tag (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     cos_id BIGINT NOT NULL,
     title VARCHAR(255) NOT NULL,
     FOREIGN KEY (cos_id) REFERENCES course(id),
 );
+
 CREATE TABLE cos_progress (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
@@ -46,6 +49,7 @@ CREATE TABLE cos_progress (
     FOREIGN KEY (user_id) REFERENCES user(id)
     FOREIGN KEY (cos_id) REFERENCES course(id),
 );
+
 CREATE TABLE orders (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
@@ -57,6 +61,7 @@ CREATE TABLE orders (
     comp VARCHAR(255) NOT NULL
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
 CREATE TABLE order_detail ( 
     ord_id BIGINT NOT NULL,
     cos_id BIGINT NOT NULL,
@@ -64,12 +69,14 @@ CREATE TABLE order_detail (
     FOREIGN(ord_id) REFERENCES order(id),
     FOREIGN(cos_id) REFERENCES course(id)
 );
+
 CREATE TABLE cart (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
     total_price BIGINT NOT NULL
     FOREIGN KEY (user_id) REFERENCES user_id
 );
+
 CREATE TABLE cart_detail (
     cart_id BIGINT NOT NULL,
     cos_id BIGINT NOT NULL,
@@ -78,6 +85,7 @@ CREATE TABLE cart_detail (
     FOREIGN KEY (cart_id) REFERENCES cart(id),
     FOREIGN KEY (cos_id) REFERENCES course(id)
 );
+
 CREATE TABLE cos_video (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     cos_id BIGINT NOT NULL
@@ -87,6 +95,7 @@ CREATE TABLE cos_video (
     link VARCHAR(3000) NOT NULL,
     FOREIGN (cos_id) REFERENCES course(id),
 );
+
 CREATE TABLE view_time (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     vid_id BIGINT NOT NULL,
@@ -94,7 +103,8 @@ CREATE TABLE view_time (
     view_length BIGINT DEFAULT 0,
     FOREIGN KEY (vid_id) REFERENCES cos_video(id),
     FOREIGN KEY (user_id) REFERENCES user(id)
-)
+);
+
 CREATE TABLE job (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -102,6 +112,7 @@ CREATE TABLE job (
     requirements VARCHAR(255) NOT NULL,
     area VARCHAR NOT NULL,
 );
+
 CREATE TABLE job_cos (
     job_id BIGINT NOT NULL,
     cos_id BIGINT NOT NULL,
@@ -109,6 +120,7 @@ CREATE TABLE job_cos (
     FOREIGN KEY (job_id) REFERENCES job(id),
     FOREIGN KEY (cos_id) REFERENCES course(id)
 );
+
 CREATE TABLE review (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
