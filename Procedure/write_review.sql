cddc1d862bd44fa7e89c3afd5f7c10b0277f4089
chohj2614 <hyunjoon0614@naver.com>
2025-12-04 18:29:15 +0900

    del uml

--- Procedure/write_review.sql
+++ Procedure/write_review.sql
@@ -3,20 +3,22 @@ CREATE PROCEDURE 리뷰 쓰기(
     IN in_user_id BIGINT,
     IN in_cos_id BIGINT,
     IN in_rating BIGINT,
-    IN in_comment
+    IN in_contents VARCHAR(255)
 )
 BEGIN
-    -- BEGIN 밑에 DECLARE를 통해 변수 선언
-    DECLARE _author_id BIGINT;
+    
+    DECLARE _ BIGINT;
     DECLARE _post_id BIGINT;
-    -- 아래 DECLARE는 변수선언과 상관없는 예외관련 특수문법
+    
+
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         ROLLBACK;
     END;
     
     START TRANSACTION;
-    -- email로 회원 id 찾기
+    -- 회원이 해당 강의를 들었는지 조회
+    
     SELECT id
     INTO _author_id
     FROM author
