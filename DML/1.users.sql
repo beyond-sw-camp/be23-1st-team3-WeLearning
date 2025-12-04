b7ad652ae5cc34013447c8a720041b7f6ababbbb
2001056 <119822421+2001056@users.noreply.github.com>
2025-12-04 18:47:11 +0900

    Update 1.users.sql

--- DML/1.users.sql
+++ DML/1.users.sql
@@ -1,29 +1,12 @@
-CREATE TABLE user (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    age BIGINT NOT NULL,
-    role EMUM('수강생','강사') NOT NULL DEFAULT '수강생'
-    phone VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE instructors (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    profile VARCHAR(255) NOT NULL,
-    career VARCHAR(3000),
-    rating DECIMAL NOT NULL,
-    students BIGINT DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES user(id)
-);
-
-CREATE TABLE cos_progress (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    cos_id BIGINT NOT NULL,
-    viewtime BIGINT NOT NULL,
-    progress BIGINT NOT NULL
-    FOREIGN KEY (user_id) REFERENCES user(id)
-    FOREIGN KEY (cos_id) REFERENCES course(id),
-);
+INSERT INTO user (email, password, name, age, role, phone) VALUES 
+('user1@mail.com', 'pass1', '강유찬', 33, '수강생', '010-9927-4702'),
+('user2@mail.com', 'pass2', '정예준', 35, '수강생', '010-3771-9881'),
+('user3@mail.com', 'pass3', '권준우', 24, '수강생', '010-4758-8629'),
+('user4@mail.com', 'pass4', '정가은', 54, '강사', '010-8485-5125'), 
+('user5@mail.com', 'pass5', '강유찬2', 32, '수강생', '010-1470-7686'),
+('user6@mail.com', 'pass6', '김민서', 22, '강사', '010-5527-2363'), 
+('user7@mail.com', 'pass7', '최하준', 28, '수강생', '010-6365-6985'),
+('user8@mail.com', 'pass8', '서민서', 47, '수강생', '010-2614-5380'),
+('user9@mail.com', 'pass9', '한하준', 39, '강사', '010-7376-4857'), 
+('user10@mail.com', 'pass10', '김지호', 36, '강사', '010-1994-1725'),
+('user11@mail.com', 'pass11', '권현호', 23, '수강생', '010-2494-4225');
